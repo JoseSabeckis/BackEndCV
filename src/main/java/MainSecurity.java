dfb09eb3acaf1dea745c4a50b72006771b36a2ba
JoseSabeckis <josesabeckis_pj@hotmail.com.ar>
2022-08-29 21:55:19 -0300

    educacion abm

--- src/main/java/MainSecurity.java
+++ src/main/java/MainSecurity.java
@@ -55,7 +55,7 @@ public class MainSecurity extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// TODO Auto-generated method stub
-	    http.cors().and().csrf().disable().authorizeRequests().antMatchers("/auth/**").permitAll()
+	    http.cors().and().csrf().disable().authorizeRequests().antMatchers("**").permitAll()
 		.anyRequest().authenticated().and().exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
 		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
